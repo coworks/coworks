ad38bfaa1111b8b879f0849e6b3fc011b09f1c91
user1 <user1@KH_H>
2019-07-18 09:30:10 +0900

    MERGE COMMIT

--- coworks/src/main/java/com/kh/coworks/board/controller/BoardController.java
+++ coworks/src/main/java/com/kh/coworks/board/controller/BoardController.java
@@ -1,8 +1,32 @@
 package com.kh.coworks.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
+	// 자료실 - 사규
+	@RequestMapping("/board/companyrulesdata.do")
+	public String companyrules() {
+		return "board/companyrules";
+	}
+
+	// 자료실 - 통합자료실 - 업무문서
+	@RequestMapping("/board/businessdocdata.do")
+	public String businessdoc() {
+		return "";
+	}
+
+	// 자료실 - 통합자료실 - 교육문서
+	@RequestMapping("/board/empedudocdata.do")
+	public String empedudoc() {
+		return "";
+	}
+
+	// 자료실 - 부서별 자료실
+	@RequestMapping("/board/deptdocdata.do")
+	public String deptdoc() {
+		return "";
+	}
 
 }
