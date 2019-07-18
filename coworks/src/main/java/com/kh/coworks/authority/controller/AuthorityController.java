a28f77cf0571898e862bf3740f1d1095faf0cc94
jun <jun@DESKTOP-5HLF4T1>
2019-07-18 15:08:58 +0900

    07-18 sangjun : authority add

--- coworks/src/main/java/com/kh/coworks/authority/controller/AuthorityController.java
+++ coworks/src/main/java/com/kh/coworks/authority/controller/AuthorityController.java
@@ -1,8 +1,13 @@
 package com.kh.coworks.authority.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthorityController {
-
+	
+	@RequestMapping("/authority/authorityList.do")
+	public String selectList() {
+		return "authority/authorityList";
+	}
 }
