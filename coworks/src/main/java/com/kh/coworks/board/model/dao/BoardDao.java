942c50e88ab5fa8e1cad79f228b11e2b91938395
user1 <user1@KH_H>
2019-07-24 13:08:27 +0900

    0723 board 수정 부분 업데이트

--- coworks/src/main/java/com/kh/coworks/board/model/dao/BoardDao.java
+++ coworks/src/main/java/com/kh/coworks/board/model/dao/BoardDao.java
@@ -12,13 +12,16 @@ public interface BoardDao {
 	/////////////////////////////////////////////////////////////////////////자료실
 	// 자료실 - 업무문서----------------------------------------------------------------
 	// 게시글 목록 조회용
-	List<Map<String, String>> selectBusinessdoc(int cPage, int limit);
+	List<Map<String, String>> selectBusinessdocList(int cPage, int limit);
 	
 	// 페이지 처리를 위한 게시글 총 개수
 	int selectBusinessdocTotalContents();
 	
 	// 게시글 추가
-	int insertBusinessdoc(Board board, List<Attach> attachList);
+	int insertBusinessdoc(Board board);
+	
+	// 첨부파일 넣기
+	int insertBusinessdocAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOnebusinessdocdetail(int boardNo); 
@@ -27,10 +30,16 @@ public interface BoardDao {
 	List<Attach> selectBusinessdocAttachList(int boardNo);
 	
 	// 게시글 수정
-	int updateBusinessdocview(Board board, List<Attach> attachList);
+	int updateBusinessdocview(Board board);
+	
+	// 첨부파일 수정
+	int updateBusinessdocAttach(Attach att);
 	
 	// 게시글 삭제
 	int deleteBusinessdoc(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteBusinessdocAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteBusinessdocFile(int attNo);
@@ -48,10 +57,13 @@ public interface BoardDao {
 	int selectCompanyrulesTotalContents();
 	
 	// 게시글 추가
-	int insertCompanyrule(Board board, List<Attach> attachList);
+	int insertCompanyrule(Board board);
+	
+	// 첨부파일 추가
+	int insertCompanyruleAttach(Attach att);
 	
 	// 게시글 내용 조회
-	Board companyruledetail(int boardNo); 
+	Board SelectOnecompanyruledetail(int boardNo); 
 	
 	// 해당 게시글 첨부파일 목록 조회
 	List<Attach> selectCompanyruleAttachList(int boardNo);
@@ -59,8 +71,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateCompanyruleview(Board board, List<Attach> attachList);
 	
+	// 게시글 첨부파일 수정
+	int updateCompanyruleAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteCompanyrule(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteCompanyruleAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteCompanyruleFile(int attNo);
@@ -79,7 +97,10 @@ public interface BoardDao {
 	int selectDeptdocTotalContents();
 	
 	// 게시글 추가
-	int insertDeptdoc(Board board, List<Attach> attachList);
+	int insertDeptdoc(Board board);
+	
+	// 첨부파일 추가
+	int insertDeptdocAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOnedeptdocdetail(int boardNo); 
@@ -90,8 +111,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateDeptdocview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateDeptdocAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteDeptdoc(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteDeptdocAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteDeptdocFile(int attNo);
@@ -111,7 +138,10 @@ public interface BoardDao {
 	int selectEmpedudocTotalContents();
 	
 	// 게시글 추가
-	int insertEmpedudoc(Board board, List<Attach> attachList);
+	int insertEmpedudoc(Board board);
+	
+	// 첨부파일 추가
+	int insertEmpedudocAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneempedudocdetail(int boardNo); 
@@ -122,8 +152,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateEmpedudocview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateEmpedudocAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteEmpedudoc(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteEmpedudocAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteEmpedudocFile(int attNo);
@@ -143,7 +179,10 @@ public interface BoardDao {
 	int selectBulletinboardTotalContents();
 	
 	// 게시글 추가
-	int insertBulletinboard(Board board, List<Attach> attachList);
+	int insertBulletinboard(Board board);
+	
+	// 첨부파일 추가
+	int insertBulletinboardAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneBulletinboarddetail(int boardNo); 
@@ -154,8 +193,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateBulletinboardview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateBulletinboardAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteBulletinboard(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteBulletinboardAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteBulletinboardFile(int attNo);
@@ -176,7 +221,10 @@ public interface BoardDao {
 	int selectDeptboardTotalContents();
 	
 	// 게시글 추가
-	int insertDeptboard(Board board, List<Attach> attachList);
+	int insertDeptboard(Board board);
+	
+	// 첨부파일 추가
+	int insertDeptboardAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneDeptboarddetail(int boardNo); 
@@ -187,8 +235,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateDeptboardview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateDeptboardAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteDeptboard(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteDeptboardAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteDeptboardFile(int attNo);
@@ -208,7 +262,10 @@ public interface BoardDao {
 	int selectFamilyeventTotalContents();
 	
 	// 게시글 추가
-	int insertFamilyevent(Board board, List<Attach> attachList);
+	int insertFamilyevent(Board board);
+	
+	// 첨부파일 추가
+	int insertFamilyeventAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneFamilyeventdetail(int boardNo); 
@@ -219,8 +276,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateFamilyeventview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateFamilyeventAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteFamilyevent(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteFamilyeventAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteFamilyeventFile(int attNo);
@@ -240,7 +303,10 @@ public interface BoardDao {
 	int selectNewsboardTotalContents();
 	
 	// 게시글 추가
-	int insertNewsboard(Board board, List<Attach> attachList);
+	int insertNewsboard(Board board);
+	
+	// 첨부파일 추가
+	int insertNewsboardAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneNewsboarddetail(int boardNo); 
@@ -251,8 +317,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateNewsboardview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateNewsboardAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteNewsboard(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteNewsboardAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteNewsboardFile(int attNo);
@@ -272,7 +344,10 @@ public interface BoardDao {
 	int selectNoticeTotalContents();
 	
 	// 게시글 추가
-	int insertNoticeform(Board board, List<Attach> attachList);
+	int insertNoticeform(Board board);
+	
+	// 첨부파일 추가
+	int insertNoticeAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneNoticedetail(int boardNo); 
@@ -283,8 +358,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateNoticeview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateNoticeAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteNotice(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteNoticeAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteNoticeFile(int attNo);
@@ -304,7 +385,10 @@ public interface BoardDao {
 	int selectReorganizationTotalContents();
 	
 	// 게시글 추가
-	int insertReorganizationform(Board board, List<Attach> attachList);
+	int insertReorganizationform(Board board);
+	
+	// 첨부파일 추가
+	int insertReorganizationAttach(Attach att);
 	
 	// 게시글 내용 조회
 	Board selectOneReorganizationdetail(int boardNo); 
@@ -315,8 +399,14 @@ public interface BoardDao {
 	// 게시글 수정
 	int updateReorganizationview(Board board, List<Attach> attachList);
 	
+	// 첨부파일 수정
+	int updateReorganizationAttach(Attach att);
+	
 	// 게시글 삭제
 	int deleteReorganization(int boardNo);
+	
+	// 첨부파일 전체 삭제
+	int deleteReorganizationAttach(int boardNo);
 	
 	// 파일 한 개 삭제
 	int deleteReorganizationFile(int attNo);
