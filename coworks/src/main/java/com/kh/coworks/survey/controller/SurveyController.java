cc4d0bd308b16c2f60c34e836defb5ff54fc84ae
jun <jun@DESKTOP-5HLF4T1>
2019-08-03 17:28:38 +0900

    0803 상준 설부 테스트

--- coworks/src/main/java/com/kh/coworks/survey/controller/SurveyController.java
+++ coworks/src/main/java/com/kh/coworks/survey/controller/SurveyController.java
@@ -1,8 +1,18 @@
 package com.kh.coworks.survey.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SurveyController {
 
+	@RequestMapping("/survey/surveyChart.do")
+	public String surveyChart() {
+		
+		
+		
+		return "survey/surveyChart";
+	}
+	
+	
 }
