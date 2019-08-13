e28fe55900b849b099cdd4cbebeb3ba7e955395f
cptsu <cptsu@DESKTOP-6S5NU5Q>
2019-08-14 07:56:00 +0900

    근태버튼수정

--- coworks/src/main/java/com/kh/coworks/attendance/controller/AttendanceController.java
+++ coworks/src/main/java/com/kh/coworks/attendance/controller/AttendanceController.java
@@ -34,172 +34,173 @@ import com.kh.coworks.employee.model.vo.Employee;
 
 @Controller
 public class AttendanceController {
-	 
-	@Autowired 
-		private AttendanceService attendanceService;
-	@Autowired 
-	private CalendarService calendarSerivce;
-	
-	SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
-	Calendar cal=new GregorianCalendar();
-	 Time time=new Time(cal.getTimeInMillis());
-	 Date date=new Date(cal.getTimeInMillis());
-	 
-	 //**** 지각 계산
-	 String str1 = new SimpleDateFormat("yyyyMMdd").format(date); 
-	 //System.out.println(str2);
-	 String reqDateStr1=str1+"090000";	//최소 출근시간 기준
-	 String reqDateStr2=str1+"180000";	//정규 퇴근시작시간
-	 String reqDateStr3=str1+"070000";	// 출근찍을수있는시간 7시~6시사이!
-	 
+    
+   @Autowired 
+      private AttendanceService attendanceService;
+   @Autowired 
+   private CalendarService calendarSerivce;
+   
+   SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
+   Calendar cal=new GregorianCalendar();
+    Time time=new Time(cal.getTimeInMillis());
+    Date date=new Date(cal.getTimeInMillis());
+    
+    //**** 지각 계산
+    String str1 = new SimpleDateFormat("yyyyMMdd").format(date); 
+    //System.out.println(str2);
+    String reqDateStr1=str1+"090000";   //최소 출근시간 기준
+    String reqDateStr2=str1+"180000";   //정규 퇴근시작시간
+    String reqDateStr3=str1+"070000";   // 출근찍을수있는시간 7시~6시사이!
+    
 
-	@RequestMapping("/mypage/attendanceleave.do") 
-	public void insertAttendaceLeave(HttpServletRequest request,HttpServletResponse response,@RequestParam() String atten_no,Model model) throws IOException, ParseException { 
-		System.out.println("atten_no값 : "+atten_no);
-		HttpSession session=request.getSession(false);
-		Employee employee=(Employee) session.getAttribute("employee"); 
-		ModelAndView mv=new ModelAndView();
-		Calendar cal=new GregorianCalendar();
-		Attendance attend=new Attendance();
-		 Time time=new Time(cal.getTimeInMillis());
-		 Date date=new Date(cal.getTimeInMillis());
-		  
-		 //** 조퇴 및 총근무시간 계산
-		 String str1 = new SimpleDateFormat("yyyyMMdd").format(date);
-		 String str2 = new SimpleDateFormat("HHmm").format(time);
-		 
-		 String reqDateStr=str1+"180000";	//최소 출근시간 기준
-		  
-		  
-		//현재시간 Date
-		java.util.Date curDate = new java.util.Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
-		
-		//요청시간을 Date로 parsing 후 time가져오기
-		java.util.Date reqDate = dateFormat.parse(reqDateStr2);	// 18:00
-		System.out.println("reqDate : "+reqDate);
-		long reqDateTime = reqDate.getTime();
-		System.out.println("longreqDate : "+reqDateTime);
-		//현재시간을 요청시간의 형태로 format 후 time 가져오기
-		curDate = dateFormat.parse(dateFormat.format(curDate));
-		
-		long curDateTime = curDate.getTime();
-		System.out.println("curDate : "+curDateTime);
-		//분으로 표현
-		if(curDateTime<reqDateTime) {
-		long hour= (reqDateTime-curDateTime) / (1000*60*60);
-		long minute = (reqDateTime-curDateTime) / 60000-(hour*60);
-		long second=(reqDateTime-curDateTime) / 1000-((hour*60*60)+(minute*60));
-		String leaveEarly=String.format("%02d:%02d:%02d", hour,minute,second); 
-			attend.setAtten_leaveEarly(leaveEarly);
-		}else {
-			attend.setAtten_leaveEarly(null);	
-		}
-		 
-		 
-		
-		 
-		attend.setAtten_leaveIP(request.getRemoteAddr());	
-		attend.setEmp_no(employee.getEmp_no());	//나중에 세션에서 받아오기
-		attend.setAtten_leaveTime(time); 	// 퇴근찍은 시간	 
-		int no=Integer.parseInt(atten_no);
-		attend.setAtten_no(no); 
-		Attendance list =attendanceService.selectOneAttendance(employee.getEmp_no());
-		
-		// 총 근무시간
-		Date datee=new Date(cal.getTimeInMillis());
-		
-		 //**** 지각 계산
-		 String hourstr = new SimpleDateFormat("yyyyMMdd").format(datee);
-		 
-		 System.out.println(reqDateStr);
+   @RequestMapping("/mypage/attendanceleave.do") 
+   public void insertAttendaceLeave(HttpServletRequest request,HttpServletResponse response,@RequestParam() String atten_no,Model model) throws IOException, ParseException { 
+      System.out.println("atten_no값 : "+atten_no);
+      HttpSession session=request.getSession(false);
+      Employee employee=(Employee) session.getAttribute("employee"); 
+      ModelAndView mv=new ModelAndView();
+      Calendar cal=new GregorianCalendar();
+      Attendance attend=new Attendance();
+       Time time=new Time(cal.getTimeInMillis());
+       Date date=new Date(cal.getTimeInMillis());
+        
+       //** 조퇴 및 총근무시간 계산
+       String str1 = new SimpleDateFormat("yyyyMMdd").format(date);
+       String str2 = new SimpleDateFormat("HHmm").format(time);
+       
+       String reqDateStr=str1+"180000";   //최소 출근시간 기준
+        
+        
+      //현재시간 Date
+      java.util.Date curDate = new java.util.Date();
+      SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
+      
+      //요청시간을 Date로 parsing 후 time가져오기
+      java.util.Date reqDate = dateFormat.parse(reqDateStr2);   // 18:00
+      System.out.println("reqDate : "+reqDate);
+      long reqDateTime = reqDate.getTime();
+      System.out.println("longreqDate : "+reqDateTime);
+      //현재시간을 요청시간의 형태로 format 후 time 가져오기
+      curDate = dateFormat.parse(dateFormat.format(curDate));
+      
+      long curDateTime = curDate.getTime();
+      System.out.println("curDate : "+curDateTime);
+      //분으로 표현
+      if(curDateTime<reqDateTime) {
+      long hour= (reqDateTime-curDateTime) / (1000*60*60);
+      long minute = (reqDateTime-curDateTime) / 60000-(hour*60);
+      long second=(reqDateTime-curDateTime) / 1000-((hour*60*60)+(minute*60));
+      String leaveEarly=String.format("%02d:%02d:%02d", hour,minute,second); 
+         attend.setAtten_leaveEarly(leaveEarly);
+      }else {
+         attend.setAtten_leaveEarly(null);   
+      }
+       
+       
+      
+       
+      attend.setAtten_leaveIP(request.getRemoteAddr());   
+      attend.setEmp_no(employee.getEmp_no());   //나중에 세션에서 받아오기
+      attend.setAtten_leaveTime(time);    // 퇴근찍은 시간    
+      int no=Integer.parseInt(atten_no);
+      attend.setAtten_no(no); 
+      Attendance list =attendanceService.selectOneAttendance(employee.getEmp_no());
+      
+      // 총 근무시간
+      Date datee=new Date(cal.getTimeInMillis());
+      
+       //**** 지각 계산
+       String hourstr = new SimpleDateFormat("yyyyMMdd").format(datee);
+       
+       System.out.println(reqDateStr);
 
-		 String all2 = new SimpleDateFormat("HHmmss").format(list.getAtten_attTime().getTime());
-		 String a= hourstr+all2;
-		  
-			System.out.println("");
-			//요청시간을 Date로 parsing 후 time가져오기
-			java.util.Date hours = dateFormat.parse(a); 
-			long atten_attTime = hours.getTime();
-			//System.out.println("longatten_hours는??"+atten_hours);
-			
-			java.util.Date current = dateFormat.parse(dateFormat.format(curDate));
-			long atten_leaveTime = current.getTime();
-			
-		 
-			//요청시간을 Date로 parsing 후 time가져오기
-			java.util.Date reqDate1 = dateFormat.parse(reqDateStr1);
-			java.util.Date reqDate2 = dateFormat.parse(reqDateStr2);
-			java.util.Date reqDate3 = dateFormat.parse(reqDateStr3);
-			System.out.println("reqDate : "+reqDate1);
-			long reqDateTime1 = reqDate1.getTime();	// 9시
-			long reqDateTime2 = reqDate2.getTime(); // 18시
-			long reqDateTime3 = reqDate3.getTime(); // 7시	
-			
-			
-		if(reqDateTime3<atten_leaveTime && reqDateTime1>atten_leaveTime) {	// 7시~9시이전 퇴근이면
-			attend.setAtten_hours(null);	//총 근무시간 안찍힘!!!!!!
-		}else if(atten_leaveTime-atten_attTime>0) {	// 출근시간-근무시간>0
-			long hour= (atten_leaveTime-atten_attTime) / (1000*60*60);
-			long minute = (atten_leaveTime-atten_attTime) / 60000-(hour*60);
-			long second=(atten_leaveTime-atten_attTime) / 1000-((hour*60*60)+(minute*60));
-			 
-			String hours2=String.format("%02d:%02d:%02d", hour,minute,second);
-			 
-				attend.setAtten_hours(hours2);
-		}
-			  
-		int result=attendanceService.updateAttendanceLeave(attend); 
-		
-		response.getWriter().print(true);
-		
-	
-	}
-	
-	@RequestMapping("/mypage/attendanceview.do")
-	public ModelAndView selectListAttendance(HttpServletRequest request) {
-		 ModelAndView mv=new ModelAndView();
-		HttpSession session=request.getSession(false);
-		Employee employee=(Employee) session.getAttribute("employee");
-		
-		List<Attendance> list=attendanceService.selectListAttendance(employee.getEmp_no());
-		
-		mv.addObject("list",list);
-		mv.setViewName("mypage/attendancetable");
-		return mv;
-	}
-	
-	 
-	  @RequestMapping("/attendance/selectTypeAttendance.do") 
-	  public ModelAndView selectOneAttendance(HttpServletRequest request,@RequestParam("select_date") String select_date ) throws ParseException { 
-		  ModelAndView mv=new ModelAndView(); 
-		  HttpSession session=request.getSession(false); 
-		  Employee employee=(Employee) session.getAttribute("employee");
-		   
-		  
-		  String year=select_date.substring(2,4);
-		  int y=Integer.parseInt(year);
-		  String month=select_date.substring(6,8);
-		  int m=Integer.parseInt(month); 
-		  String date1="20"+year+month+"01";  
-		  Attendance attend=new Attendance();
-		  attend.setEmp_no(employee.getEmp_no());
-		  
-		  // uilt.Date를 sql.Date로 변경
-		  SimpleDateFormat dt = new SimpleDateFormat("yyyyMMdd"); 
-		  java.util.Date date = dt.parse(date1);  
-		  java.sql.Date sqlDate = new java.sql.Date(date.getTime());
-		 // DateFormat df = new SimpleDateFormat("YYYY-MM-DD");
-	   //     System.out.println("dateFormated date is : " + df.format(utilDate));
-		  attend.setAtten_date(sqlDate); 
-			   
-		  List<Attendance> list=attendanceService.selectTypeAttendance(attend);
-	  
-	  mv.addObject("list",list); 
-	  mv.setViewName("mypage/attendancetable");
-	  return mv; 
-	  
-	  }
+       String all2 = new SimpleDateFormat("HHmmss").format(list.getAtten_attTime().getTime());
+       String a= hourstr+all2;
+        
+         System.out.println("");
+         //요청시간을 Date로 parsing 후 time가져오기
+         java.util.Date hours = dateFormat.parse(a); 
+         long atten_attTime = hours.getTime();
+         //System.out.println("longatten_hours는??"+atten_hours);
+         
+         java.util.Date current = dateFormat.parse(dateFormat.format(curDate));
+         long atten_leaveTime = current.getTime();
+         
+       
+         //요청시간을 Date로 parsing 후 time가져오기
+         java.util.Date reqDate1 = dateFormat.parse(reqDateStr1);
+         java.util.Date reqDate2 = dateFormat.parse(reqDateStr2);
+         java.util.Date reqDate3 = dateFormat.parse(reqDateStr3);
+         System.out.println("reqDate : "+reqDate1);
+         long reqDateTime1 = reqDate1.getTime();   // 9시
+         long reqDateTime2 = reqDate2.getTime(); // 18시
+         long reqDateTime3 = reqDate3.getTime(); // 7시   
+         
+         
+      if(reqDateTime3<atten_leaveTime && reqDateTime1>atten_leaveTime) {   // 7시~9시이전 퇴근이면
+         attend.setAtten_hours(null);   //총 근무시간 안찍힘!!!!!!
+      }else if(atten_leaveTime-atten_attTime>0) {   // 출근시간-근무시간>0
+         long hour= (atten_leaveTime-atten_attTime) / (1000*60*60);
+         long minute = (atten_leaveTime-atten_attTime) / 60000-(hour*60);
+         long second=(atten_leaveTime-atten_attTime) / 1000-((hour*60*60)+(minute*60));
+          
+         String hours2=String.format("%02d:%02d:%02d", hour,minute,second);
+          
+            attend.setAtten_hours(hours2);
+      }
+           
+      int result=attendanceService.updateAttendanceLeave(attend); 
+      
+      response.getWriter().print(true);
+      
+   
+   }
+   
+   @RequestMapping("/mypage/attendanceview.do")
+   public ModelAndView selectListAttendance(HttpServletRequest request) {
+       ModelAndView mv=new ModelAndView();
+      HttpSession session=request.getSession(false);
+      Employee employee=(Employee) session.getAttribute("employee");
+      
+      List<Attendance> list=attendanceService.selectListAttendance(employee.getEmp_no());
+      
+      mv.addObject("select_date","날짜 선택");
+      mv.addObject("list",list);
+      mv.setViewName("mypage/attendancetable");
+      return mv;
+   }
+   
+    
+     @RequestMapping("/attendance/selectTypeAttendance.do") 
+     public ModelAndView selectOneAttendance(HttpServletRequest request,@RequestParam("select_date") String select_date ) throws ParseException { 
+        ModelAndView mv=new ModelAndView(); 
+        HttpSession session=request.getSession(false); 
+        Employee employee=(Employee) session.getAttribute("employee");
+         
+        
+        String year=select_date.substring(2,4);
+        int y=Integer.parseInt(year);
+        String month=select_date.substring(6,8);
+        int m=Integer.parseInt(month); 
+        String date1="20"+year+month+"01";  
+        Attendance attend=new Attendance();
+        attend.setEmp_no(employee.getEmp_no());
+        
+        // uilt.Date를 sql.Date로 변경
+        SimpleDateFormat dt = new SimpleDateFormat("yyyyMMdd"); 
+        java.util.Date date = dt.parse(date1);  
+        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+       // DateFormat df = new SimpleDateFormat("YYYY-MM-DD");
+      //     System.out.println("dateFormated date is : " + df.format(utilDate));
+        attend.setAtten_date(sqlDate); 
+            
+        List<Attendance> list=attendanceService.selectTypeAttendance(attend);
+    mv.addObject("select_date",select_date);
+     mv.addObject("list",list); 
+     mv.setViewName("mypage/attendancetable");
+     return mv; 
+     
+     }
 
 
  
--- coworks/src/main/webapp/WEB-INF/views/mypage/attendancetable.jsp
+++ coworks/src/main/webapp/WEB-INF/views/mypage/attendancetable.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8" import="java.util.*, java.text.SimpleDateFormat"%>
+   pageEncoding="UTF-8" import="java.util.*, java.text.SimpleDateFormat"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
@@ -11,12 +11,12 @@
 <c:import url="../common/header.jsp" />
 </head>
 <body class="fix-header fix-sidebar card-no-border">
-	<div id="main-wrapper">
-		<c:import url="../common/topbar.jsp" />
-		<c:import url="../common/sidebar.jsp" />
-		<div class="page-wrapper">
-			<div class="container-fluid">
-				<div class="row page-titles">
+   <div id="main-wrapper">
+      <c:import url="../common/topbar.jsp" />
+      <c:import url="../common/sidebar.jsp" />
+      <div class="page-wrapper">
+         <div class="container-fluid">
+            <div class="row page-titles">
                     <div class="col-md-6 col-8 align-self-center"> 
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="javascript:void(0)">메인페이지</a></li>
@@ -27,135 +27,135 @@
 
 
 
-				<div class="col-12">
-					<div class="card">
-						<div class="card-body">
+            <div class="col-12">
+               <div class="card">
+                  <div class="card-body">
+ 
+                     <div class="align-self-center">
+                        <h3 class="card-title float-left">${sessionScope.employee.emp_name}
+                           님의 근태 현황</h3><br>
+                             
+                        <div class=" align-self-center">
+                         
+                           <div class="dropdown float-right ">
+                              <button class="btn btn-secondary dropdown-toggle"
+                                 type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                                 aria-haspopup="true" aria-expanded="false" >${select_date}</button>
+                              <div class="dropdown-menu"
+                                 aria-labelledby="dropdownMenuButton">
+                                 <input type="button" class="dropdown-item" id="month1"
+                                    name="month1"
+                                    onclick="fnonclick($('input[name=month1]').val())" /> <input
+                                    type="button" class="dropdown-item" id="month2"
+                                    name="month2"
+                                    onclick="fnonclick($('input[name=month2]').val())" /> <input
+                                    type="button" class="dropdown-item" id="month3"
+                                    name="month3"
+                                    onclick="fnonclick($('input[name=month3]').val())" />
+                              </div>
+                           </div>
+                        </div>
+
+                     </div>
+                     <script>
+                        $(function() {
+                           var date = new Date();
+                           if (date.getMonth() < 10) {
+                              var m = '0' + (date.getMonth() + 1);
+                           } else {
+                              var m = date.getMonth() + 1;
+                           }
+                           var y = date.getFullYear();
+                           var form = '';
+                           var today = new Date($.now());
+
+                           console.log(date);
+                           $('input[name=month1]').val(
+                                 y + "년 " + m + "월");
+                           if (m == 1) {
+                              m = 12;
+                           } else {
+                              if (m < 11)
+                                 m = '0' + (m - 1);
+                              else
+                                 m = m - 1;
+                           }
+                           ;
+                           $('input[name=month2]').val(
+                                 y + "년 " + m + "월");
+                           if (m == 1) {
+                              m = 12;
+                           } else {
+                              if (m < 11)
+                                 m = '0' + (m - 1);
+                              else
+                                 m = m - 1;
+                           }
+                           ;
+                           $('input[name=month3]').val(
+                                 y + "년 " + m + "월");
+
+                        });
+                        function fnonclick(date) {
+
+                           var d = date;
+
+                           location.href = "${pageContext.request.contextPath}/attendance/selectTypeAttendance.do?select_date="
+                                 + date;
+
+                        };
+                     </script>
+                     </br>
+                     </br>
+                     </br>
+                     </br>
+                        
+                     <div class="table-responsive">
+                        <table class="table no-wrap">
+                           <thead>
+                              <tr>
+                                 <th></th>
+                                 <th>날짜</th>
+                                 <th>출근</th>
+                                 <th>퇴근</th>
+                                 <th>지각</th>
+                                 <th>조퇴</th>
+                                 <th>근무시간</th>
+                                 <th>출근 IP</th>
+                                 <th>퇴근 IP</th>
+                              </tr>
+                           </thead>
+                           <tbody>
+                              <c:forEach items="${list}" var="attend" varStatus="idx">
+                              
+                                 <tr>
+                                    <td>${idx.count}
+                                    <td>${attend.atten_date}</td>
+                                    <td>${attend.atten_attTime}</td>
+                                    <td>${attend.atten_leaveTime}</td>
+                                    <td>${attend.atten_attLate}</td>
+                                    <td>${attend.atten_leaveEarly}</td>
+                                    <td>${attend.atten_hours}</td>
+                                    <td>${attend.atten_attIP}</td>
+                                    <td>${attend.atten_leaveIP}</td>
+                                 </tr>
+                              </c:forEach>
+
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+
+            </div>
 
 
-							<div class="align-self-center">
-								<h4 class="card-title float-left">${sessionScope.employee.emp_name}
-									님의 근태 현황</h4> 
-								<div class=" align-self-center"> 
-									<div class="dropdown float-right ">
-										<button class="btn btn-secondary dropdown-toggle"
-											type="button" id="dropdownMenuButton" data-toggle="dropdown"
-											aria-haspopup="true" aria-expanded="false">날짜 선택</button>
-										<div class="dropdown-menu"
-											aria-labelledby="dropdownMenuButton">
-											<input type="button" class="dropdown-item" id="month1"
-												name="month1"
-												onclick="fnonclick($('input[name=month1]').val())" /> <input
-												type="button" class="dropdown-item" id="month2"
-												name="month2"
-												onclick="fnonclick($('input[name=month2]').val())" /> <input
-												type="button" class="dropdown-item" id="month3"
-												name="month3"
-												onclick="fnonclick($('input[name=month3]').val())" />
-										</div>
-									</div>
-								</div>
-
-							</div>
-							<script>
-								$(function() {
-									var date = new Date();
-									if (date.getMonth() < 10) {
-										var m = '0' + (date.getMonth() + 1);
-									} else {
-										var m = date.getMonth() + 1;
-									}
-									var y = date.getFullYear();
-									var form = '';
-									var today = new Date($.now());
-
-									console.log(date);
-									$('input[name=month1]').val(
-											y + "년 " + m + "월");
-									if (m == 1) {
-										m = 12;
-									} else {
-										if (m < 11)
-											m = '0' + (m - 1);
-										else
-											m = m - 1;
-									}
-									;
-									$('input[name=month2]').val(
-											y + "년 " + m + "월");
-									if (m == 1) {
-										m = 12;
-									} else {
-										if (m < 11)
-											m = '0' + (m - 1);
-										else
-											m = m - 1;
-									}
-									;
-									$('input[name=month3]').val(
-											y + "년 " + m + "월");
-
-								});
-								function fnonclick(date) {
-
-									var d = date;
-
-									location.href = "${pageContext.request.contextPath}/attendance/selectTypeAttendance.do?select_date="
-											+ date;
-
-								};
-							</script>
-							</br>
-							</br>
-							</br>
-							</br>
 
 
-							<div class="table-responsive">
-								<table class="table no-wrap">
-									<thead>
-										<tr>
-											<th></th>
-											<th>날짜</th>
-											<th>출근</th>
-											<th>퇴근</th>
-											<th>지각</th>
-											<th>조퇴</th>
-											<th>근무시간</th>
-											<th>출근 IP</th>
-											<th>퇴근 IP</th>
-										</tr>
-									</thead>
-									<tbody>
-										<c:forEach items="${list}" var="attend" varStatus="idx">
-										
-											<tr>
-												<td>${idx.count}
-												<td>${attend.atten_date}</td>
-												<td>${attend.atten_attTime}</td>
-												<td>${attend.atten_leaveTime}</td>
-												<td>${attend.atten_attLate}</td>
-												<td>${attend.atten_leaveEarly}</td>
-												<td>${attend.atten_hours}</td>
-												<td>${attend.atten_attIP}</td>
-												<td>${attend.atten_leaveIP}</td>
-											</tr>
-										</c:forEach>
-
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-
-				</div>
-
-
-
-
-			</div>
-			<c:import url="../common/footer.jsp" />
-		</div>
-	</div>
-	<c:import url="../common/bottomJquery.jsp" />
+         </div>
+         <c:import url="../common/footer.jsp" />
+      </div>
+   </div>
+   <c:import url="../common/bottomJquery.jsp" />
 </body>
 </html>
